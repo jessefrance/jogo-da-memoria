3e521b27aa7a741e71ca9364f4c4972371168a6c
jessefrance <jesseluis09@gmail.com>
2022-10-27 21:04:27 -0300

    add 'card options'.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,37 @@ document.addEventListener('DOMContentLoaded', () => {
     // opções de cards//
     const cardArray [
         {
-            
+            name:'xbox-360',
+            img:'.images/mBox_360.png',  
+        },
+        {
+            name:'xbox-one',
+            img:'.images/mBox_one.png',  
+        },
+        {
+            name:'mega-drive',
+            img:'.images/mega-drive.png',  
+        },
+        {
+            name:'nintendo-64',
+            img:'.images/nintendo-64.png',  
+        },
+        {
+            name:'platform-2v3',
+            img:'.images/Platform2V3.png',  
+        },
+        {
+            name:'play-system',
+            img:'.images/Playsystem.png',  
+        },
+        {
+            name:'ps4',
+            img:'.images/ps4.png',  
+        },
+        {
+            name:'super-nintendo',
+            img:'.images/super-nintendo.png',  
         }
+
     ]
 })
