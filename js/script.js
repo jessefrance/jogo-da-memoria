3064d47e76c938d8b92eed44f46f8aff3b9b43b6
jessefrance <jesseluis09@gmail.com>
2022-10-27 21:09:14 -0300

    add cardArray

--- js/script.js
+++ js/script.js
@@ -7,8 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
             img:'.images/mBox_360.png',  
         },
         {
+            name:'xbox-360',
+            img:'.images/mBox_360.png',  
+        },
+        {
             name:'xbox-one',
             img:'.images/mBox_one.png',  
+        },
+        {
+            name:'xbox-one',
+            img:'.images/mBox_one.png',  
+        },
+        {
+            name:'mega-drive',
+            img:'.images/mega-drive.png',  
         },
         {
             name:'mega-drive',
@@ -19,6 +31,14 @@ document.addEventListener('DOMContentLoaded', () => {
             img:'.images/nintendo-64.png',  
         },
         {
+            name:'nintendo-64',
+            img:'.images/nintendo-64.png',  
+        },
+        {
+            name:'platform-2v3',
+            img:'.images/Platform2V3.png',  
+        },
+        {
             name:'platform-2v3',
             img:'.images/Platform2V3.png',  
         },
@@ -27,8 +47,20 @@ document.addEventListener('DOMContentLoaded', () => {
             img:'.images/Playsystem.png',  
         },
         {
+            name:'play-system',
+            img:'.images/Playsystem.png',  
+        },
+        {
             name:'ps4',
             img:'.images/ps4.png',  
+        },
+        {
+            name:'ps4',
+            img:'.images/ps4.png',  
+        },
+        {
+            name:'super-nintendo',
+            img:'.images/super-nintendo.png',  
         },
         {
             name:'super-nintendo',
