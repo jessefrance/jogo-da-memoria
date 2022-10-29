cdc29eb1393addaeb4f88a793d2e948f027b738e
jessefrance <jesseluis09@gmail.com>
2022-10-29 16:54:26 -0300

    finalizando p script

--- js/script.js
+++ js/script.js
@@ -1,110 +1,117 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    // opções de cards//
-    const cardArray [
-        {
-            name:'xbox-360',
-            img:'.images/mBox_360.png',  
-        },
-        {
-            name:'xbox-360',
-            img:'.images/mBox_360.png',  
-        },
-        {
-            name:'xbox-one',
-            img:'.images/mBox_one.png',  
-        },
-        {
-            name:'xbox-one',
-            img:'.images/mBox_one.png',  
-        },
-        {
-            name:'mega-drive',
-            img:'.images/mega-drive.png',  
-        },
-        {
-            name:'mega-drive',
-            img:'.images/mega-drive.png',  
-        },
-        {
-            name:'nintendo-64',
-            img:'.images/nintendo-64.png',  
-        },
-        {
-            name:'nintendo-64',
-            img:'.images/nintendo-64.png',  
-        },
-        {
-            name:'platform-2v3',
-            img:'.images/Platform2V3.png',  
-        },
-        {
-            name:'platform-2v3',
-            img:'.images/Platform2V3.png',  
-        },
-        {
-            name:'play-system',
-            img:'.images/Playsystem.png',  
-        },
-        {
-            name:'play-system',
-            img:'.images/Playsystem.png',  
-        },
-        {
-            name:'ps4',
-            img:'.images/ps4.png',  
-        },
-        {
-            name:'ps4',
-            img:'.images/ps4.png',  
-        },
-        {
-            name:'super-nintendo',
-            img:'.images/super-nintendo.png',  
-        },
-        {
-            name:'super-nintendo',
-            img:'.images/super-nintendo.png',  
-        }
-
+    //card options
+    const cardArray = [
+      {
+        name: 'mbox-360',
+        img: 'images/mBox_360.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      },
+      {
+        name: 'fries',
+        img: 'images/fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      }
     ]
-})
-
-// Criação de Placa //
-const grid = document.querySelector('.grid')
-var cardsChosen = []
-var cardsChosenId = []
-
-function creatBoard() {
-    for(let i = 0; i < cardArray.length; i++) {
-        var card = document.createElement('img')
-        card.setAttribute('src', '.images/mBox_360.png')
+  
+    cardArray.sort(() => 0.5 - Math.random())
+  
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId = []
+    let cardsWon = []
+  
+    //create your board
+    function createBoard() {
+      for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
-        // card.addEventListener('click', flipCard)
+        card.addEventListener('click', flipCard)
         grid.appendChild(card)
+      }
     }
-}
-
-// check for matches
-function checkForMatch(){
-    var cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
-    if (cardsChosen[0] === cardsChosen[1]) {
-        alert('Parábens, você achou o par!')
-        cards[optionOneId].setAttribute('src', '.images/super-nintendo.png')
-    }   cards[optionTwoId].setAttribute('src', '.images/super-nintendo.png')
-}
-
-// flip you card
-function flipCard() {
-   var cardId = this.getAttribute ('data-id')
-   cardsChosen.push(cardArray[cardId].name)
-   cardsChosenId.push(cardId)
-   this.setAttribute('src', cardArray[cardId].img)
-   if (cardsChosen.length === 2) {
+  
+    //check for matches
+    function checkForMatch() {
+      const cards = document.querySelectorAll('img')
+      const optionOneId = cardsChosenId[0]
+      const optionTwoId = cardsChosenId[1]
+      
+      if(optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You have clicked the same image!')
+      }
+      else if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+      } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sorry, try again')
+      }
+      cardsChosen = []
+      cardsChosenId = []
+      resultDisplay.textContent = cardsWon.length
+      if  (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations! You found them all!'
+      }
+    }
+  
+    //flip your card
+    function flipCard() {
+      let cardId = this.getAttribute('data-id')
+      cardsChosen.push(cardArray[cardId].name)
+      cardsChosenId.push(cardId)
+      this.setAttribute('src', cardArray[cardId].img)
+      if (cardsChosen.length ===2) {
         setTimeout(checkForMatch, 500)
-   }
-}
-
-creatBoard()
+      }
+    }
+  
+    createBoard()
+  })
+  
